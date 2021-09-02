25fb90283346610a6f8bd879645c503d48bac19f
mivo <michel.vorwieger@outlook.de>
2021-09-02 14:11:09 +0200

    Initial commit

---
+++ src/main/java/com/vorwieger/tictactoetddkata/TicTacToeTddKataApplication.java
@@ -0,0 +1,13 @@
+package com.vorwieger.tictactoetddkata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicTacToeTddKataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicTacToeTddKataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vorwieger/tictactoetddkata/TicTacToeTddKataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vorwieger.tictactoetddkata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicTacToeTddKataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
